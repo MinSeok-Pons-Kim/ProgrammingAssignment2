56eefb7e2981162531163922dbb814e5cf904edb
MinSeok-Pons-Kim <singiman2@hanmail.net>
2016-08-22 11:58:20 +0900

    Modify getting row info for reversing matrix in cache Solve

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,21 @@
 makeCacheMatrix <- function(x = matrix()) {
     inv <- NULL
+    
     set <- function(y) {
         x <<- y
         inv <<- NULL
     }
+    
     get <- function() x
+    
     set_inverse <- function(inverse_matrix) inv <<- inverse_matrix
+    
     get_inverse <- function() inv
+    
     list(set = set, get = get,
         set_inverse = set_inverse,
         get_inverse = get_inverse)
+        
 }
 
 
@@ -17,19 +23,23 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
     ## Return a matrix that is the inverse of 'x'
+    
     inverse_matrix <- x$get_inverse()
-    if(!is.null(inv)){
+    
+    if(!is.null(inverse_matrix)){
         message("getting inverse matrix")
         return(inverse_matrix)
     }
     
-    inverse_matrix <- matrix(nrow <- nrow(x$get(x)), ncol <- 0)
+    inverse_matrix <- matrix(nrow <- nrow(x), ncol <- 0)
     # or you can replace as matrix(nrow <- dim(x$get(x))[1], ncol <- 0)
     for(n in nrow(x)){ # stuff data inverse
         inverse_matrix <- cbind(inverse_matrix, a[,n])
     }
     #set inverse_matrix
     x$set_inverse(inverse_matrix)
+    
+    #return inverse_matrix
     inverse_matrix
     
 }
